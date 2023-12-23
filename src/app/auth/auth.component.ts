b351630f3704b562aa72abbaa5eae908c798e506
Ivan Cherkas <cherkas.ivan13@gmail.com>
2023-12-23 09:40:59 +0300

    first commit

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'npx-cv-ssr-auth',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './auth.component.html',
+    styleUrl: './auth.component.scss',
+})
+export class AuthComponent {}
